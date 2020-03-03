b759578cd290ffac118988efd34a2bf86a8b87b4
Mathan K A <amheretoinspire@hotmail.com>
2020-03-03 15:54:26 +0530

    updated code

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const http = require('http');
-const port = 3000;
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-});
+const http = require('http')
+const port = 3000
 
-server.listen(port, err => {
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
+
   console.log(`server is listening on ${port}`)
 })
