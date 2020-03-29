6ad370081d32f082155ecdb89dbaa56db3629332
Mathan K A <mathan@cyware.com>
2020-03-29 20:40:58 +0530

    Add Blog route added

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const port = process.env.PORT || 1337;
 const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
+router.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname +  '/public/index.html'));
 });
 
-app.get('/blog', (req, res) => {
-})
+router.get('/blog',(req,res) => {
+  res.sendFile(path.join(__dirname + '/public/blog.html'));
+});
+
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html');
+// });
+
+// app.get('/blog', (req, res) => {
+//   res.sendFile('blog.html');
+// })
+
+app.use(express.static(__dirname + '/public'));
+app.use('/', router);
 
 app.listen(port);
