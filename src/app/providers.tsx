0a27545ec284375e6ee9a6f9a8a8f7b25bca9048
Mathan K A <amheretoinspire@hotmail.com>
2025-12-24 07:48:48 +0530

    chore(posthog-proxy): :hammer: revert: proxy reverted to debug later

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,17 +6,17 @@ import { PostHogProvider as PHProvider } from "@posthog/react";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    const isLocalhost =
-      typeof window !== "undefined" &&
-      (window.location.hostname === "localhost" ||
-        window.location.hostname === "127.0.0.1");
+    // const isLocalhost =
+    //   typeof window !== "undefined" &&
+    //   (window.location.hostname === "localhost" ||
+    //     window.location.hostname === "127.0.0.1");
 
-    if (isLocalhost) return;
+    // if (isLocalhost) return;
     if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) return;
 
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-      api_host: "/_mka",
-      ui_host: "https://us.posthog.com",
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      // ui_host: "https://us.posthog.com",
       defaults: "2025-11-30",
       capture_pageview: "history_change",
       respect_dnt: true,
@@ -26,12 +26,12 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
         }
       }
     });
-    if (
-      location.hostname === "localhost" ||
-      location.hostname === "127.0.0.1"
-    ) {
-      posthog.opt_out_capturing();
-    }
+    // if (
+    //   location.hostname === "localhost" ||
+    //   location.hostname === "127.0.0.1"
+    // ) {
+    //   posthog.opt_out_capturing();
+    // }
   }, []);
 
   return <PHProvider client={posthog}>{children}</PHProvider>;
